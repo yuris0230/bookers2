954b9308c660b65ab8d71ca3d51b529f204c4ab1
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-23 15:10:27 +0000

    [Edit] routes homes > home/about, Bugs 13 > 9

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  get "homes/about", to: "homes#about", as: "about"
+  get "home/about", to: "homes#about", as: "about"
 
   resources :users, only: [:index, :show, :edit, :update]
   resources :books
