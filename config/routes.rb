d8dbf7b48083cb7f10b3f096f98b228dc479cfce
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-23 00:22:18 +0000

    [Create] model, [Add] book's user_id column, User:Book model 1:N, header, [Fix] config/initializers/devise : login via email > name, some routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+
+  resources :users, only:[:index, :show, :edit, :update]
+  resources :books
+  get "/homes/about" => "homes#about", as: "about"
 end
