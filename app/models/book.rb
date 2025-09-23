3fa9640e14c9bf13d23823960c2292d0dbeb18a0
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-23 15:06:42 +0000

    [Add] models/book validates, Bugs 19 > 13

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+  # ===== Validation =====
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
 end
