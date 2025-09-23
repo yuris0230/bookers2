9e8c4a666abe9a40ffd7d453b609cd3b188be391
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-23 14:34:15 +0000

    [Add] model/user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
   def get_profile_image(width, height)
     unless profile_image.attached?
