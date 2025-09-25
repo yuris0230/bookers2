39ecab3043397e7285d1477d8a5724797c8f8260
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-25 05:49:05 +0000

    [Fix] delete table and body from add_columns_to_books becuz already exist in create_books

--- db/migrate/20250922222638_add_columns_to_books.rb
+++ db/migrate/20250922222638_add_columns_to_books.rb
@@ -1,7 +1,5 @@
 class AddColumnsToBooks < ActiveRecord::Migration[6.1]
   def change
-    add_column :books, :title, :string
-    add_column :books, :body, :text
     add_reference :books, :user, null: false, foreign_key: true
   end
 end
