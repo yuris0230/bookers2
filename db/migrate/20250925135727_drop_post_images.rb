8c1aa03346722af875790d09474d2fcb1cb1afac
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-25 14:01:26 +0000

    [Delete] no need post_image and lists tables, [Edit] app_controller, del :name from permitted_parameters

---
+++ db/migrate/20250925135727_drop_post_images.rb
@@ -0,0 +1,5 @@
+class DropPostImages < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :post_images
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_09_23_031809) do
+ActiveRecord::Schema.define(version: 2025_09_25_135756) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,16 +47,6 @@ ActiveRecord::Schema.define(version: 2025_09_23_031809) do
     t.text "body"
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
-  end
-
-  create_table "lists", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "post_images", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
